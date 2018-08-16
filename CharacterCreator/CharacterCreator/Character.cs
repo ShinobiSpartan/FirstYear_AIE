d4fb762a39222433410255e9149950543e3a9395
ShinobiSpartan <36467680+ShinobiSpartan@users.noreply.github.com>
2018-08-16 14:50:20 +1000

    - Started Character Creator Tutorial

---
+++ CharacterCreator/CharacterCreator/Character.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CharacterCreator
+{
+    class Character
+    {
+        public override string ToString()
+        {
+            return base.ToString() + "\n\tpath: \t" + spritesheet.path + "\n\ttile coordiantes: \t" + tileCoordinates.ToString();
+        }
+
+        public string name;
+
+        public Spritesheet spritesheet;
+
+        public Point tileCoordinates = new Point(0, 0);
+
+        public Character(string name, Spritesheet spritesheet)
+        {
+            this.name = name;
+            this.spritesheet = spritesheet;
+        }
+    }
+}
---
+++ CharacterCreator/CharacterCreator/Spritesheet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CharacterCreator
+{
+    class Spritesheet
+    {
+        public override string ToString()
+        {
+            return base.ToString() + ": " + path.ToString();
+        }
+
+        private Image image = null;
+
+        public string path;
+
+        public int GetWidth()
+        {
+            return 0;
+        }
+
+        public int GetHeight()
+        {
+            return 0;
+        }
+
+        public Spritesheet(string path)
+        {
+            this.path = path;
+            Load();
+        }
+
+        public void Load()
+        {
+            image = Image.FromFile(path);
+        }
+    }
+}
